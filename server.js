b2228566c2d1f5b7dd06f737dbc7201612f2d00c
ocampco <mark.ocampo@rea-group.com>
2017-05-27 00:08:25 +1000

    Compress files

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const compression = require('compression');
 const express = require('express');
 const app = express();
 
+app.use(compression());
 app.use(express.static(__dirname + '/public'));
 
 app.set('port', (process.env.PORT || 5000));
