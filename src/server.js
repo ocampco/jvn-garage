efcab0c6c346fceec2abaf52d6ac99b5f0f58d18
ocampco <mark.ocampo@rea-group.com>
2017-08-04 23:26:48 +1000

    Move files to src folder

--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 
 app.use(compression());
-app.use(express.static(__dirname.resolve('/public')));
+app.use(express.static(`${__dirname}/public`));
 
 app.set('port', (process.env.PORT || 5000));
-app.set('views', __dirname.resolve('/views'));
+app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
